0c21773e07c404056d730fcf72ccae394cc3e8f1
Nicholas W. Heyer <60236014+nickheyer@users.noreply.github.com>
2021-07-16 04:17:08 -0700

    INSPECT ME

--- DiscoFlix.py
+++ DiscoFlix.py
@@ -5,6 +5,7 @@ import json
 import os
 import io
 from radarr_api_v3 import RadarrAPIv3
+from sonarr_api import SonarrAPI
 from datetime import date
 import sys
 
@@ -20,10 +21,14 @@ except:
   startup_val["disc_token"] = input("Please enter your Discord API token here: ") ; sleep(1)
   startup_val["rad_token"] = input("Please enter your Radarr API token here: ") ; sleep(1)
   startup_val["rad_host"] = input("Please enter your Radarr's Host Url (include port number as well): ") ; sleep(1)
+  startup_val["enable_son"] = input("Would you like to enable TV-Show requests with Sonarr? ('yes' or 'no') ")
+  if startup_val["enable_son"].lower().strip() == "yes":
+    startup_val["son_token"] = input("Please enter your Sonarr API token here: ") ; sleep(1)
+    startup_val["son_host"] = input("Please enter your Sonarr's Host Url (include port number as well): ") ; sleep(1) 
   startup_val["tmdb_token"] = input("Please enter your TMDB API token here: ") ; sleep(1)
   startup_val["s_name"] = input("Please enter the name of your media server here: ") ; sleep(1)
   startup_val["s_host"] = input("Please enter your discord username here (with discord id number at the end, ex: NastyNick#4212): ") ; sleep(1)
-  startup_val["avg_d_time"] = input("Please enter the average time it takes (in seconds) for a movie to download (using quality preset 'any') here: ") ; sleep(1)
+  startup_val["avg_d_time"] = input("Please enter the average time it takes (in minutes) for a movie to download (using quality preset 'any') here: ") ; sleep(1)
   with open(INI_PATH, "w") as ini_values:
     json.dump(startup_val, ini_values)
 
@@ -32,15 +37,21 @@ TOKEN = startup_val["disc_token"]
 #Radarr API Token
 R_TOKEN = startup_val["rad_token"]
 #Radarr Host Url
-HOST_URL = startup_val["rad_host"]
+R_HOST_URL = startup_val["rad_host"]
 #Radarr API Var
-radarr = RadarrAPIv3(HOST_URL, R_TOKEN)
+radarr = RadarrAPIv3(R_HOST_URL, R_TOKEN)
+#Radarr API Token
+S_TOKEN = startup_val["son_token"]
+#Sonarr Host Url
+S_HOST_URL = startup_val["son_host"]
+#Sonarr API Var
+sonarr = SonarrAPI(S_HOST_URL, S_TOKEN)
 #TMDB API Token
 TMDB_TOKEN = startup_val["tmdb_token"]
-#Movie request command
-keyword = "!df request"
+#Request command
+keyword = "!df"
 #Last search json file
-last_movies = os.path.join(os.path.dirname(__file__), f'last_movies.json')
+last_search = os.path.join(os.path.dirname(__file__), f'last_search.json')
 #Set your server name to be referenced in message responses
 SERVER_NAME = startup_val["s_name"]
 #Set your discord name to be referenced in message responses
@@ -60,8 +71,6 @@ except:
     json.dump(auth_users, auth_user_json)
 
 
-#Storing of dl logs
-LOGS_PATH = os.path.join(os.path.dirname(__file__), f'LOGS\\{date.today()}.txt')
 
 client = discord.Client()
 embed = discord.Embed()
@@ -81,8 +90,8 @@ async def on_message(message):
   current_channel = message.channel.id
   if message.author == client.user:
     return
-  elif message.content.lower().startswith(keyword.lower()) and full_user in auth_users.values():
-    req_movie = message.content.lower()[len(keyword):].strip()
+  elif message.content.lower().startswith(f"{keyword.lower()} movie") and full_user in auth_users.values():
+    req_movie = message.content.lower()[(len(keyword) + 6):].strip()
     str_req_movie = req_movie
     await message.channel.send(f"Searching for {req_movie.title()}") ; sleep(1)
     await message.channel.send(f"One moment...") ; sleep(1)
@@ -112,17 +121,18 @@ async def on_message(message):
     for x in html["results"]:
         x["release_date"] = x.get("release_date", "Release date unavailable")
         x["title"] = x.get("title", "Title unavailable")
-        x["poster_path"] = x.get("poster_path", "/dykOcAqI01Fci5cKQW3bEUrPWwU.jpg")
+        poster_suffix = x.get("poster_path", "/dykOcAqI01Fci5cKQW3bEUrPWwU.jpg")
+        x["poster_path"] = (f"https://image.tmdb.org/t/p/original{poster_suffix}")
         x["overview"] = x.get("overview", "Description unavailable.")
         if x["overview"] == "":
           x["overview"] = "Description unavailable."
-        if x["poster_path"] == "Null":
-          x["poster_path"] = "/dykOcAqI01Fci5cKQW3bEUrPWwU.jpg"
+        if "jpg" not in x["poster_path"]:
+          x["poster_path"] = "https://i.imgur.com/1glpRCZ.png?1"
         print_list[x["original_title"]] = [x["id"], x["poster_path"], x["title"], x["release_date"], x["overview"]]
-    with io.open(last_movies, "w", encoding = "UTF-8") as last_movies_w:
-        json.dump(print_list, last_movies_w)
-    with io.open(last_movies, "r", encoding = "UTF-8") as last_movies_r:
-        list_of_movies = json.load(last_movies_r)
+    with io.open(last_search, "w", encoding = "UTF-8") as last_search_w:
+        json.dump(print_list, last_search_w)
+    with io.open(last_search, "r", encoding = "UTF-8") as last_search_r:
+        list_of_movies = json.load(last_search_r)
     selected_movie_id = ""
     number_of_results = len(list_of_movies.keys())
     count_1 = 0
@@ -130,7 +140,7 @@ async def on_message(message):
     while True:
       for x,y in list_of_movies.items():
           await message.channel.send("Is this the correct movie? ('yes' or 'no')") ; sleep(1)
-          embed.set_image(url=f"https://image.tmdb.org/t/p/original{y[1]}")
+          embed.set_image(url= y[1])
           await message.channel.send(embed=embed)
           await message.channel.send(f"`{y[2]} ({y[3]})`\n```{y[4]}```")
           player_choice = await client.wait_for('message', check=lambda message: message.author == current_requester and message.channel.id == current_channel and message.content.lower().strip() in ["yes", "y", "startover", "stop", "no", "n", ])
@@ -157,6 +167,8 @@ async def on_message(message):
         break
       elif player_choice.content.lower().strip() == "startover":
         pass
+    #Storing of dl logs
+    LOGS_PATH = os.path.join(os.path.dirname(__file__), f'LOGS\\{date.today()}.txt')
     og_stdout = sys.stdout
     try:  
       with io.open(LOGS_PATH, "a", encoding = "UTF-8") as logs_file:
@@ -176,9 +188,85 @@ async def on_message(message):
         await message.channel.send(f"Movie is being downloaded to {SERVER_NAME}.") ; sleep(1)
         await message.channel.send(f"Please wait: {avg_time_download} seconds...") ; sleep(avg_time_download)
         await message.channel.send(f"If movie is not currently on {SERVER_NAME}, please contact {ADMIN_NAME}.")
+  elif message.content.lower().startswith(f"{keyword.lower()} tvshow") and full_user in auth_users.values() and startup_val["enable_son"].lower().strip() == "yes":
+    req_show = message.content.lower()[(len(keyword) + 7):].strip()
+    str_req_show = req_show.title()
+    google_search_req = ""
+    for x in req_show:
+      if x == " ":
+        google_search_req += "+"
+      else:
+        google_search_req += x      
+    await message.channel.send(f"Searching for {req_show.title()}") ; sleep(1)
+    await message.channel.send(f"One moment...") ; sleep(1)
+    show_search = (sonarr.lookup_series(f"'{req_show}'"))[:10]
+    count_1 = 0
+    while True:
+      for x in show_search: 
+          await message.channel.send("Is this the correct show? ('yes' or 'no')") ; sleep(1)
+          try:
+            embed.set_image(url= x["images"][1]["url"])
+          except:
+            embed.set_image(url="https://i.imgur.com/1glpRCZ.png?1")
+          await message.channel.send(embed=embed)
+          await message.channel.send(f"`{x['title']} ({x['year']})`\n```{x['overview']}```")
+          player_choice = await client.wait_for('message', check=lambda message: message.author == current_requester and message.channel.id == current_channel and message.content.lower().strip() in ["yes", "y", "startover", "stop", "no", "n", ])
+          if player_choice.content.lower().strip() == "yes" or player_choice.content.lower().strip() == "y":
+            await message.channel.send(f"Selected: `{x['title']} ({x['year']})`") ; sleep(2)
+            selected_show_title = x['title']
+            selected_show_id = x['tvdbId']
+            break
+          elif player_choice.content.lower().strip() == "startover":
+            await message.channel.send(f"Starting search over...") ; sleep(1)
+            break
+          elif player_choice.content.lower().strip() == "stop":
+            await message.channel.send(f"Cancelling search... Have a good day!") ; sleep(1)
+            return
+          elif player_choice.content.lower().strip() == "no" or player_choice.content.lower().strip() == "n":
+            count_1 += 1
+            if count_1 == len(show_search):
+              await message.channel.send(f"Unfortunately, we have run out of results.") ; sleep(1)
+              await message.channel.send(f"It's possible that this show does not exist, let's check if it does and try again...") ; sleep(1)
+              await message.channel.send(f"https://letmegooglethat.com/?q={google_search_req}%3F")
+              return
+            else:
+              pass
+      if player_choice.content.lower().strip() == "yes" or player_choice.content.lower().strip() == "y":
+        break
+      elif player_choice.content.lower().strip() == "startover":
+        pass
+    #Getting rootdir path
+    rootdir = sonarr.get_root()[0]['path']
+    #Storing of dl logs
+    LOGS_PATH = os.path.join(os.path.dirname(__file__), f'LOGS\\{date.today()}.txt')
+    og_stdout = sys.stdout
+    try:  
+      with io.open(LOGS_PATH, "a", encoding = "UTF-8") as logs_file:
+        sys.stdout = logs_file
+        print(f"{message.author.name}#{message.author.discriminator} requested:\n")
+        add_show = sonarr.add_series(selected_show_id, 1, rootdir)
+        print(sonarr.lookup_series(selected_show_id))
+        sys.stdout = og_stdout
+    except:
+      with io.open(LOGS_PATH, "w", encoding = "UTF-8") as logs_file:
+        sys.stdout = logs_file
+        print(f"{message.author.name}#{message.author.discriminator} requested:\n")
+        add_show = sonarr.add_series(selected_show_id, 1, rootdir)
+        print(sonarr.lookup_series(selected_show_id))
+        sys.stdout = og_stdout
+    if str(type(add_show)) == "<class 'list'>":
+        await message.channel.send(f"Looks like this show is already available on {SERVER_NAME}, if not, please contact {ADMIN_NAME}.")
+    else:
+        await message.channel.send(f"Show is being downloaded to {SERVER_NAME}.") ; sleep(1)
+        await message.channel.send(f"Please wait while the show is being downloaded, check server frequently.")
+        s_id = 0
+        for x in sonarr.get_series():
+          if x['title'] == selected_show_title:
+                s_id = x['id']
+        sonarr.set_command(name = "missingEpisodeSearch", seriesId = s_id)
   elif message.content.startswith("!df set keyword") and full_user in auth_users.values():
     keyword = message.content.strip()[13:]
-    await message.channel.send(f"You can now request movies with '{keyword}'.")
+    await message.channel.send(f"You can now request movies and tv-shows with '{keyword}'.")
   elif message.content.lower().startswith("!df add user") and full_user in auth_users.values():
     added_user = message.content.strip()[12:]
     count = 1
@@ -189,7 +277,7 @@ async def on_message(message):
     with open(AUTH_USER_PATH, "w") as auth_user_json:
       json.dump(auth_users, auth_user_json)
   elif message.content.lower().startswith("!df help") and full_user in auth_users.values():
-        await message.channel.send(f"'{keyword}'\n'!df set keyword <new keyword>'\n'!df add user <user>'\n\n'For more help, visit https://github.com/nickheyer/DiscoFlix'")
+        await message.channel.send(f"'{keyword} movie'\n'{keyword} tvshow'\n'!df set keyword <new keyword>'\n'!df add user <user>'\n\n'For more help, visit https://github.com/nickheyer/DiscoFlix'")
   else:
     return
 
