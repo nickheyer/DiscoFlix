dce5b37334ef08f743c37360f21f12cda1b2461f
Nicholas Heyer <nick@heyer.app>
2023-08-22 22:19:53 -0700

    Generated app

---
+++ DiscoFlix/asgi.py
@@ -0,0 +1,23 @@
+"""
+ASGI config for DiscoFlix project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DiscoFlix.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    # Just HTTP for now
+})
---
+++ DiscoFlixClient/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DiscoflixclientConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'DiscoFlixClient'
