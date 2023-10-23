546a9d530967b776a3b41768fcfedcbe98801be7
Nicholas Heyer <nick@heyer.app>
2023-10-22 19:08:56 -0700

    Disabling csrf mw. Admin panel doesnt like it.

--- DiscoFlix/settings.py
+++ DiscoFlix/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
